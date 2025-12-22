69484575c9cdf9bda1b9b6c0b11cfca07e6acb45
ian-dungan <iandungan@me.com>
2025-12-22 16:28:45 -0600

    Update SupabaseClient.js

--- src/supabase/SupabaseClient.js
+++ src/supabase/SupabaseClient.js
@@ -1,2 +1,2 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-export const supabase = createClient('https://YOUR_PROJECT.supabase.co','YOUR_PUBLIC_ANON_KEY');
+export const supabase = createClient('https://depvgmvmqapfxjwkkhas.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlcHZnbXZtcWFwZnhqd2traGFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5NzkzNzgsImV4cCI6MjA4MDU1NTM3OH0.WLkWVbp86aVDnrWRMb-y4gHmEOs9sRpTwvT8hTmqHC0eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlcHZnbXZtcWFwZnhqd2traGFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5NzkzNzgsImV4cCI6MjA4MDU1NTM3OH0.WLkWVbp86aVDnrWRMb-y4gHmEOs9sRpTwvT8hTmqHC0');
