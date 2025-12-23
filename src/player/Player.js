c7e414a4cc9b1d5fda113c6c8f1a5914798cf2a4
ian-dungan <iandungan@me.com>
2025-12-23 09:57:38 -0600

    Refactor Player class to extend Phaser.Sprite

--- src/player/Player.js
+++ src/player/Player.js
@@ -1,19 +1,24 @@
-export default class Player {
+export default class Player extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y, texture) {
-    this.scene = scene;
-    this.user_id = 'local';
-    this.sprite = scene.physics.add.sprite(x, y, texture, 0);
-    this.sprite.setCollideWorldBounds(true);
-    this.cursors = scene.input.keyboard.createCursorKeys();
-    this.speed = 200;
+    super(scene, x, y, texture);
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+    this.setCollideWorldBounds(true);
+    this.speed = 150;
+    this.stepSound = scene.sound.add('step', { volume: 0.2 });
   }
-  update() {
-    const { cursors, sprite, speed } = this;
-    sprite.body.setVelocity(0);
-    if (cursors.left.isDown) sprite.body.setVelocityX(-speed);
-    else if (cursors.right.isDown) sprite.body.setVelocityX(speed);
-    if (cursors.up.isDown) sprite.body.setVelocityY(-speed);
-    else if (cursors.down.isDown) sprite.body.setVelocityY(speed);
-    sprite.body.velocity.normalize().scale(speed);
+
+  handleInput(keys) {
+    let vx = 0, vy = 0;
+    if (keys.W.isDown) vy = -this.speed;
+    else if (keys.S.isDown) vy = this.speed;
+    if (keys.A.isDown) vx = -this.speed;
+    else if (keys.D.isDown) vx = this.speed;
+    this.setVelocity(vx, vy);
+    if (vx || vy) this.playStep();
+  }
+
+  playStep() {
+    if (!this.stepSound.isPlaying) this.stepSound.play();
   }
 }
