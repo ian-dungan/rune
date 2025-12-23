b93ed9a3869cc020f16cb8873d2a49143692b53b
ian-dungan <iandungan@me.com>
2025-12-23 12:30:54 -0600

    Refactor WorldScene for new player and ground setup

--- src/scenes/WorldScene.js
+++ src/scenes/WorldScene.js
@@ -1,36 +1,22 @@
-import Player from '../player/Player.js';
-import MobileControls from '../player/MobileControls.js';
+import * as Phaser from '../lib/phaser.esm.js';
 
 export default class WorldScene extends Phaser.Scene {
-  constructor() { super('WorldScene'); }
+  constructor() {
+    super('WorldScene');
+  }
 
   preload() {
-    this.load.image('tileset', 'assets/images/tileset.png');
-    this.load.tilemapTiledJSON('town', 'assets/maps/town.json');
-    this.load.spritesheet('knight', 'assets/images/knight.png', { frameWidth: 32, frameHeight: 32 });
-    this.load.audio('ambient', 'assets/audio/ambient.mp3');
-    this.load.audio('step', 'assets/audio/step_grass.wav');
+    this.load.image('ground', 'assets/images/ui_parchment.png'); // placeholder
   }
 
-  create({ username }) {
-    this.sound.play('ambient', { loop: true, volume: 0.4 });
-    const map = this.make.tilemap({ key: 'town' });
-    const tileset = map.addTilesetImage('tileset', 'tileset');
-    map.createLayer('Ground', tileset, 0, 0);
+  create(data) {
+    console.log('🌍 Entering Rune world as', data.username);
+    this.add.text(50, 50, `Welcome, ${data.username}!`, {
+      font: '24px serif',
+      color: '#fff'
+    });
 
-    this.player = new Player(this, 400, 300, 'knight');
-    this.cameras.main.startFollow(this.player);
-    this.keys = this.input.keyboard.addKeys('W,A,S,D');
-
-    if (/Mobi|Android/i.test(navigator.userAgent)) {
-      new MobileControls(this, this.player);
-      console.log('📱 Mobile controls active');
-    }
-
-    this.add.text(16, 16, `Welcome, ${username}`, { fontSize: '20px', color: '#fff' }).setScrollFactor(0);
-  }
-
-  update(t, delta) {
-    if (!/Mobi|Android/i.test(navigator.userAgent)) this.player.update(this.keys, delta);
+    const ground = this.add.image(this.scale.width / 2, this.scale.height / 2, 'ground');
+    ground.setDisplaySize(this.scale.width, this.scale.height);
   }
 }
