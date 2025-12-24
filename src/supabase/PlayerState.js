bad79c4a1f53c9420b7ef4c1c7ca730f0ca626cd
ian-dungan <iandungan@me.com>
2025-12-24 13:30:58 -0600

    Delete src directory

--- src/supabase/PlayerState.js
+++
@@ -1,4 +0,0 @@
-import { supabase } from './SupabaseClient.js';
-export async function getOrCreatePlayerState(user_id, world_id){return {pos_x:2000,pos_y:2000,pos_z:0};}
-export async function updatePlayerPosition(user_id, world_id, pos_x, pos_y){}
-export function subscribeToPlayerStates(world_id, callback){}
